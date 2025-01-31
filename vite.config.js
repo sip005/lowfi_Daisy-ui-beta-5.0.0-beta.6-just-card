b4f55d1dabda229f105be9f2b83a4ad6d3bf5415
ubuntu <test@gmailnot.com>
2025-01-31 09:39:19 +0000

    simple setup beta

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Bind the server to all network interfaces
+    port: 3000,       // Set the desired port
+  },
+})
